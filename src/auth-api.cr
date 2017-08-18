0174fde404dc3893d1331ef6c55c21dd8da98e85
Alberto Bellotti <abellott@redhat.com>
2017-08-18 18:05:22 -0400

    Added support for the auth-api service in the httpd pod.
    - Added the auth-api crystal/kemal source code.
    - Updated Dockerfile to build and install the auth-api service.
    - Remove the auth-api source and crystal upon completion.

---
+++ src/auth-api.cr
@@ -0,0 +1,7 @@
+require "kemal"
+require "json"
+require "dbus"
+require "./auth-api/version.cr"
+require "./auth-api/utils.cr"
+require "./auth-api/user.cr"
+Kemal.run
---
+++ src/auth-api/version.cr
@@ -0,0 +1,3 @@
+module Auth::Api
+  VERSION = "0.1.0"
+end
